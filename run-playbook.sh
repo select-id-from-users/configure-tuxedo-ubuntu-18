aa9127efea6719bbc8a10c1c56d8dc06547cd4a6
select-id-from-users <dmitrii@tutti.ch>
2019-05-30 12:24:26 +0200

    Do not install ansible

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt install ansible
-
 ansible-playbook configure-ubuntu-18-workstation.yml --connection=local --inventory 127.0.0.1, --become --verbose --extra-vars git_user_name=select-id-from-users --extra-vars git_user_email=${USER}@tutti.ch
